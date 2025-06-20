825d6f1a76c51e2b02763cc7d935e5e9d0301d4b
Alejandro Benítez Bravo <a01712835@tec.mx>
2025-06-20 11:09:02 -0600

    Add files via upload

---
+++ theme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "wnkz/monoglow.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      on_colors = function(colors)
+        colors.glow = "#89b4fa"
+      end
+    },
+  }
+
+}
